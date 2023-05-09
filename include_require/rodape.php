3ec118a3396590aee9b8329269f3398ce298a3a9
poison <106964549+poisonrin@users.noreply.github.com>
2023-05-09 08:45:25 -0300

    Update rodape.php

--- include_require/rodape.php
+++ include_require/rodape.php
@@ -1,4 +1,3 @@
 <?php
-    echo "<h4>&copy; 2023 - desenvolvido por Ana - todos os direitos reservados</h5>";
     echo "<p style= 'color:red'><marquee direction='left'>Obrigado pela visita</marquee></p>";
 ?>
